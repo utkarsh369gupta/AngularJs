5910cd44d51e2315b059caa9fc3199310fe57430
Utkarsh369Gupta <utk369gupta@gmail.com>
2024-04-11 16:05:26 +0530

    added view

--- application/app.js
+++ application/app.js
@@ -1,19 +1,56 @@
 
 
-var MyApp = angular.module("myapp", [])
+var MyApp = angular.module("myapp", ['ngRoute']);
 // .filter('sortINt', function (a,b) { return a-b })
+
+
+MyApp.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider
+        .when('/home', {
+            templateUrl: 'views/homepage.html'
+        })
+        .when('/directory', {
+            templateUrl: 'views/directory.html',
+            controller: 'NinjaController'
+        }).otherwise({
+            redirectTo: '/home'
+        });
+}])
+
 
 MyApp.controller('NinjaController', ['$scope', '$http', function ($scope, $http) {
 
+
     $scope.message = 'hello my name utkarsh gupta';
+
     $http.get('ninjas.json')
         .then(function (response) {
             $scope.ninjas = response.data;
+            console.log(angular.toJson($scope.ninjas));
         })
         .catch(function (error) {
             $scope.ninjas = null;
         });
-    $scope.type = 'typeof(ninjas.name)';
+
+    $scope.removeNinja = function (ninja) {
+        var removeNinja = $scope.ninjas.indexOf(ninja);
+        $scope.ninjas.splice(removeNinja, 1);
+    }
+
+    $scope.addNinja = function () {
+        $scope.ninjas.push({
+            name: $scope.newNinja.name,
+            rate: $scope.newNinja.rate,
+            belt: $scope.newNinja.belt,
+            available: true,
+            src: ""
+        })
+
+        $scope.newNinja.name = ""
+        $scope.newNinja.rate = ""
+        $scope.newNinja.belt = ""
+    }
 
 
 }]);
